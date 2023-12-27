c5b7a4438c786729dbceadbad66078cb6623ef8b
jidori3288 <beagledog@naver.com>
2023-12-27 19:34:24 +0900

    [feat] metata.js fix

--- metadata.js
+++ metadata.js
@@ -1,13 +1,35 @@
-// metadata.js
 define(function() {
     return {
         module: {
             "zama": {
                 "version": "v1",
-                "repository": "https://github.com/your-repository",
-                "lastUpdated": "2023-04-01T12:00:00Z"
+                "repository": "",
+                "lastUpdated": ""
             }
         },
-        repo: ""
+        repo: "",
+        jupyter: {
+            version : {
+                "IPython": "8.2.0",
+                "ipykernel": "6.9.1",
+                "ipywidgets": "7.6.5",
+                "jupyter_client": "6.1.12",
+                "jupyter_core": "4.9.2",
+                "jupyter_server": "1.13.5",
+                "jupyterlab": "3.3.2",
+                "nbclient": "0.5.13",
+                "nbconvert": "6.4.4",
+                "nbformat": "5.3.0",
+                "notebook": "6.4.8",
+                "qtconsole": "5.3.0",
+                "traitlets": "5.1.1"
+            }
+        },
+        python : {
+            version : "3.8.10"
+        }
+    
+
+
     };
 });
